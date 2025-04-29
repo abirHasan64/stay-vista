7d6532281e784b8b0fee813ae3c738984fca0968
Md. Abir Hasan <155450128+abirHasan64@users.noreply.github.com>
2025-04-29 19:38:53 +0600

    query string added to front end

--- client/src/components/Categories/CategoryBox.jsx
+++ client/src/components/Categories/CategoryBox.jsx
@@ -1,18 +1,24 @@
 import PropTypes from "prop-types";
-
+import queryString from "query-string";
+import { useNavigate, useSearchParams } from "react-router-dom";
 const CategoryBox = ({ label, icon: Icon }) => {
+  const [params, setParams] = useSearchParams();
+  const category = params.get("category");
+  const navigate = useNavigate();
+  const handleClick = () => {
+    let currentQuerry = { category: label };
+    const url = queryString.stringifyUrl({
+      url: "/",
+      query: currentQuerry,
+    });
+    navigate(url);
+  };
   return (
     <div
-      className={`flex 
-  flex-col 
-  items-center 
-  justify-center 
-  gap-2
-  p-3
-  border-b-2
-  hover:text-neutral-800
-  transition
-  cursor-pointer`}
+      onClick={() => handleClick()}
+      className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer ${
+        category === label && "border-b-neutral-800 text-neutral-600"
+      } `}
     >
       <Icon size={26} />
       <div className="text-sm font-medium">{label}</div>
